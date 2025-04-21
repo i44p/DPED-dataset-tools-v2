e960b4d57ffcfe3463d332aef3ef153a7acab0a8
i44p <jsbbdjzjsjawk@gmail.com>
2025-04-21 18:39:51 +0300

    add device.py: implement abstract Device and ImageDTO

---
+++ devices/device.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+import io
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class ImageDTO:
+    jpeg: Optional[io.BytesIO]
+    raw: Optional[io.BytesIO]
+
+
+class Device(ABC):
+    @abstractmethod
+    def take_photo(self) -> ImageDTO:
+        raise NotImplementedError
+    
+    @abstractmethod
+    def get_name(self) -> str:
+        raise NotImplementedError
+
