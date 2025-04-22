c3d18e49a6043eccfb5363800315003fe48d7e0f
i44p <jsbbdjzjsjawk@gmail.com>
2025-04-22 20:14:39 +0300

    feat: implement MockSony(Device) almost fully

--- devices/sony.py
+++ devices/sony.py
@@ -9,14 +9,20 @@ class MockSony(Device):
                  *args
                  ) -> None:
         super().__init__()
+        #os.environ["LD_LIBRARY_PATH"] = "./third_party/sony-camera-example-v2-linux/out/lib"
         self._name = name
 
+        self._bus = 3
+        self._dev = 45
+
+        os.system(f"sh ./third_party/sony-camera-example-v2-linux/out/bin/0_open_session.sh --bus={self._bus} --dev={self._dev}")
 
     def prepare(self) -> None:
-        os.system("sh ./third_party/sony-camera-example-v2-linux/out/bin/focus_camera.sh --bus=003 --dev=009")
+        os.system(f"sh ./third_party/sony-camera-example-v2-linux/out/bin/1_focus_camera.sh --bus={self._bus} --dev={self._dev}")
 
     def take_photo(self) -> ImageDTO:
-        os.system("sh ./third_party/sony-camera-example-v2-linux/out/bin/shoot_an_image_and_get_it.sh --bus=003 --dev=009")
+        time.sleep()
+        os.system(f"sh ./third_party/sony-camera-example-v2-linux/out/bin/3_take_photo.sh --bus={self._bus} --dev={self._dev}")
 
         with open("./third_party/sony-camera-example-v2-linux/out/bin/shot.jpg", "rb") as f:
             jpg = io.BytesIO(f.read())
@@ -26,6 +32,9 @@ class MockSony(Device):
             raw=None
         )
         return photo
+    
+    def __del__(self) -> None:
+        os.system(f"sh ./third_party/sony-camera-example-v2-linux/out/bin/4_exit_session.sh --bus={self._bus} --dev={self._dev}")
         
 
     @property
