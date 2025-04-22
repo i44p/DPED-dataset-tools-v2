3e9eb9de2e3e51b8625c2e768b5237e6c722c8fd
Ivan <karaberov.ivan@yandex.ru>
2025-04-22 12:56:37 +0000

    feat: implement take photos method in Server

--- server.py
+++ server.py
@@ -1,4 +1,23 @@
-from devices.device import Device
+import os
+import io
+import logging
+from uuid import uuid4
+from typing import Optional
+from dataclasses import fields
+from dataclasses import dataclass
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+from devices.device import Device, ImageDTO
+from devices.kvadra import Kvadra
+from devices.camera import Camera
+
+log = logging.getLogger(__name__)
+
+
+@dataclass
+class DeviceImageDTO:
+    device_name: str
+    image: ImageDTO
 
 
 class Server:
@@ -11,13 +30,59 @@ class Server:
     def detach(self, device_name: str) -> Device | None:
         return self.attached_devices.pop(device_name)
     
-    def take_photos(self) -> None:
-        for name, device in self.attached_devices.items():
-            device.take_photo()
+    def take_photos(self) -> Optional[str]:
+        device_images: list[DeviceImageDTO] = []
+
+        with ThreadPoolExecutor() as executor:
+            futures = {
+                executor.submit(device.take_photo): name 
+                for name, device in self.attached_devices.items()
+            }
+
+            for future in as_completed(futures):
+                device_name = futures[future]
+                try:    
+                    image = future.result()
+                    device_images.append(DeviceImageDTO(device_name=device_name, image=image))
+                except Exception as e:
+                    log.error("Failed Take Photo from device `%s`. Exception > %s", device_name, e)
+                    return None
+                
+            return self._safe_photo_storage(device_images)
+    
+    def _safe_photo_storage(self, device_images: list[DeviceImageDTO]) -> Optional[str]:
+        photo_id = str(uuid4())
+
+        for device in device_images:
+            if all( [getattr(device.image, field.name) is None for field in fields(device.image)]):
+                log.error("Device `%s` returned an empty image object.", device.device_name)
+                return None
+    
+            os.makedirs(device.device_name, exist_ok=True)
+            
+            for field in fields(device.image):
+                image_bytes = getattr(device.image, field.name)
+
+                if image_bytes is not None:
+                    file_name = f"{photo_id}.{field.name}"
+                    self._save_photo(image_bytes, file_name, device.device_name)
+
+        return photo_id
+
+    def _save_photo(self, img: io.BytesIO, name: str, path: str) -> None:
+        img.seek(0)
+        with open(os.path.join(path, name), "wb") as file:
+            file.write(img.read())
 
 
 def main():
+    kvadra = Kvadra("kvadra")
+    camera = Camera("camera")
+
     server = Server()
+    server.attach(kvadra)
+    server.attach(camera)
+    server.take_photos()
     
 
 if __name__ == '__main__':
